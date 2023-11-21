7bf7c64d221d5387c24f12b97775ecec5433597a
one_punch_man <ckim@banlaw.com>
2023-11-21 13:29:46 +1100

    hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,6 +9,7 @@ import {
 import DevImg from "./DevImg";
 import Link from "next/link";
 import Socials from "./Socials";
+import Badge from "./Badge";
 
 const Hero = () => {
   return (
@@ -41,7 +42,33 @@ const Hero = () => {
               iconsStyles="text-foreground text-[22px] hover:text-primary transition-all"
             />
           </div>
-          <div className="hidden xl:flex relative">image</div>
+          <div className="hidden xl:flex relative">
+            <Badge
+              containerStyles="absolute top-[24%] -left-[5rem]"
+              icon={<RiBriefcase4Fill />}
+              endCountNum={3}
+              badgeText="Years Of Experience"
+            />
+            <Badge
+              containerStyles="absolute top-[80%] -left-[1rem]"
+              icon={<RiTodoFill />}
+              endCountNum={6}
+              endCountText="k"
+              badgeText="Finished Projects"
+            />
+            <Badge
+              containerStyles="absolute top-[55%] -right-8"
+              icon={<RiTeamFill />}
+              endCountNum={9}
+              endCountText="k"
+              badgeText="Happy Clients"
+            />
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
+            <DevImg
+              containerStyles="bg-hero_shape w-[510px] h-[462px] bg-no-repeat relative bg-bottom"
+              imgSrc="/hero/developer.png"
+            />
+          </div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />
