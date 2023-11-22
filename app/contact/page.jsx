9169749ffc9776547fbe569e7e789312fd4f4bc2
one_punch_man <ckim@banlaw.com>
2023-11-22 14:44:04 +1100

    contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,43 @@
+import Form from "@/components/Form";
+import { HomeIcon, MailIcon, PhoneCall } from "lucide-react";
+
 const Contact = () => {
-  return <div>Contact page</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+          <div className="flex flex-col justify-center">
+            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+              <span className="w-[30px] h-[2px] bg-primary"></span>
+              Say Hello 👍
+            </div>
+            <h1 className="h1 max-w-md mb-8">Let&apos;s Work Together.</h1>
+            <p className="subtitle max-w-[400px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+          </div>
+          <div className="hidden xl:flex bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
+        </div>
+        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+            <div className="flex items-center gap-x-8">
+              <MailIcon size={18} className="text-primary" />
+              <div>youremail@gmail.com</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <HomeIcon size={18} className="text-primary" />
+              <div>8420 Pine Avenue, Meadowville MC 842</div>
+            </div>
+            <div className="flex items-center gap-x-8">
+              <PhoneCall size={18} className="text-primary" />
+              <div>+44 123 456 7890</div>
+            </div>
+          </div>
+          <Form />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
